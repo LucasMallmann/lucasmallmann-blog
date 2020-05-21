75859395d1569475a2a3e8a35997a97e563aed60
lucasmallmann <lucasmallmann76@gmail.com>
2020-05-20 22:24:10 -0300

    chore: removed page creation to test posts

--- src/templates/post/index.js
+++ src/templates/post/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const BlogTemplate = ({ data }) => {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+const BlogTemplate = () => {
+  // const { markdownRemark } = data; // data.markdownRemark holds your post data
+  // const { frontmatter, html } = markdownRemark;
 
-  const { image, title, date } = frontmatter;
+  // const { image, title, date } = frontmatter;
 
   return (
     <div className="blog-post-container">
-      <div className="blog-post">
+      <h1>blog</h1>
+      {/* <div className="blog-post">
         <h1>{title}</h1>
         <h2>{date}</h2>
         <img src={image} alt="cover" />
@@ -17,28 +18,28 @@ const BlogTemplate = ({ data }) => {
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export const pageQuery = graphql`
-  query PostPage($slug: String! = "/first-post/") {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
-      timeToRead
-      excerpt
-      frontmatter {
-        title
-        date(formatString: "DD/MM/YYYY")
-        tags
-        description
-      }
-      fields {
-        slug
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query PostPage($slug: String! = "/first-post/") {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       html
+//       timeToRead
+//       excerpt
+//       frontmatter {
+//         title
+//         date(formatString: "DD/MM/YYYY")
+//         tags
+//         description
+//       }
+//       fields {
+//         slug
+//       }
+//     }
+//   }
+// `;
 
 export default BlogTemplate;
